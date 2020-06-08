20203690b97119ccb4fb3cf7c2eab2c4100bf979
Jerzy-Pawlik <jerzypawlik345+github@gmail.com>
2020-06-08 11:23:03 +0100

    Dodano funkcję dodawania zadań

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const Koa = require('koa')
 const serve = require('koa-static')
+const parse = require('koa-bodyparser')
 
 const app = new Koa()
 const port = process.env.PORT || 3000
